203ce521783d0c6f2e650880f482f1056a565d20
saugat1070 <saugatgiri1070@gmail.com>
2025-12-27 00:17:05 +0545

    First day source files

---
+++ module1/file1.js
@@ -0,0 +1,65 @@
+//console
+console.log("hey its me Saugat");
+console.error("Eya Khatra cha hae");
+console.table([[12,12,12],[10,12,13]]);
+console.clear();
+console.warn("Eya na aaija hae")
+
+// Keyword, variable and DataTypes
+
+// console.log(x);
+console.log(y);
+let x=12;
+var y = 10;
+
+//Datatypes
+console.log(typeof("Saugat"));
+console.log(typeof(123));
+console.log(typeof("1"+1));
+console.log(typeof("1"*1+1));
+console.log(typeof(undefined));
+//--> Boolean
+//--> Object
+
+//Operators
+// -->Arithemetic Operator
+
+// 5 + 5 = 10     // Addition
+// 10 - 5 = 5     // Subtraction
+// 5 * 10 = 50    // Multiplication
+// 10 / 5 = 2     // Division
+// 10 % 5 = 0     // Modulo
+
+// Comparison Operator
+
+1 > 3; // false
+3 > 1; // true
+250 >= 250; // true
+1 === 1; // true
+1 === 2; // false
+1 === '1'; // false
+
+// Logical Operator
+true && true; // true
+1 > 2 && 2 > 1; // false
+true && false; // false
+4 === 4 && 3 > 1; // true 
+
+//Terniary operator
+
+// condition ? true : false;
+
+const num = 23;
+
+if (num%2==0){
+    console.log(`${num} is even`);
+}else{
+    console.log(`${num} is odd`)
+}
+
+num % 2 == 0 ? console.log(`${num} is even`) : console.log(`${num} is odd`);
+
+
+
+
+
