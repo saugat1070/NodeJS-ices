915350ae8e3c541d4a5eb4bf79a20b03b773ac4f
saugat1070 <saugatgiri1070@gmail.com>
2026-01-09 21:59:39 +0545

    server created

---
+++ module2/index.js
@@ -0,0 +1,12 @@
+// Server create Garne
+//you have to install express from npmjs.com
+
+const express = require("express");
+
+const app = express();
+
+app.listen(3000,()=>{ // portNumber and callback function
+    console.log("Server created Successfully!!!!");
+    console.log("I have to change someting on code");
+    console.log("mah chae yatai chu")
+})
